eedcc6cbe1d35ab716f87bf7beb32069d591b1ee
cangozpi <cangozpinar@gmail.com>
2023-06-12 19:35:36 +0300

    makefile extended with experimentts section.

--- pytorch_training_loop.py
+++ pytorch_training_loop.py
@@ -101,47 +101,7 @@ def train_for_num_epochs_in_pytorch_loop(train_dataloader, model, num_epochs, lr
             logger.log_msg_to_console(f'Saving model checkpoint to path: {ckpt_path+f"/{epoch}"}, val_loss: {val_loss}')
             logger.log_to_file(f'Saving model checkpoint to path: {ckpt_path+f"/{epoch}"}, val_loss: {val_loss}')
 
-    
-
-    # Evaluate Model
-    # import evaluate
-    # metric = evaluate.load("accuracy")
-    # model.eval()
-    # for batch in validation_dataset:
-    #     # batch = {k: v.to(device) for k, v in batch.items()}
-    #     with torch.no_grad():
-    #         outputs = model(**batch)
-
-    #     logits = outputs.logits
-    #     predictions = torch.argmax(logits, dim=-1)
-    #     metric.add_batch(predictions=predictions, references=batch["labels"])
-
-    # metric.comute()
-
     return model
-
-# def evaluate_(tokenized_test, model):
-#     import evaluate
-#     accuracy_metric = evaluate.load("accuracy")
-#     precision_metric = evaluate.load("precision")
-#     recall_metric = evaluate.load("recall")
-#     bleu = evaluate.load("bleu")
-#     f1_metric = evaluate.load("f1")
-#     model.eval()
-#     for batch in tokenized_test:
-# #       batch = {k: v.to(device) for k, v in batch.items()}
-#         with torch.no_grad():
-#              outputs = model(**batch)
-#              logits = outputs.logits
-#              predictions = torch.argmax(logits, dim=-1)
-#              accuracy_result = accuracy_metric.compute(predictions=predictions, references=batch["labels"])
-#              precision_result = precision_metric.compute(predictions=predictions, references=batch["labels"])
-#              recall_result = recall_metric.compute(predictions=predictions, references=batch["labels"])
-#              bleu_result = bleu.compute(predictions=predictions, refrences=batch["labels"])
-#              f1_result = f1_metric.compute(predictions=predictions, refrences=batch["labels"])
-
-#         print("accuracy: {}, precision {}, recall {}, bleu {}, f1 {}".format(accuracy_result, precision_result, recall_result, bleu_result, f1_result))
-
 
 
 
